9694650c970efe1edcebce19a1f696bcd4a1b075
Kay Ousterhout <kayo@yahoo-inc.com>
2013-09-01 11:34:40 -0700

    Added file to run TPCH queries

---
+++ src/main/scala/shark/SparrowTPCHRunner.scala
@@ -0,0 +1,70 @@
+package shark.sparrow
+
+import shark.SharkContext
+import java.util.concurrent.ScheduledThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+import shark.SharkEnv
+import spark.Logging
+
+/**
+ * Given a workload file, runs TPCH queries at a regular rate.
+ *
+ * Expects MASTER and sparrow.app.name to be in env.
+ */
+object SparrowTPCHRunner {
+  def main(args: Array[String]) {
+    if (args.size != 2) {
+      println("Expecting file name and query rate in arguments.")
+      System.exit(-1);
+    }
+    val startDelayMs = 300 * 1000
+    val t0 = System.currentTimeMillis()
+    val source = scala.io.Source.fromFile(args(0))
+    val lines = source.mkString
+    source.close()
+
+    val delayMs = args(1).toInt
+
+    // We start with warm up queries
+    val warmUpDelayMs = 2000
+    val warmUpQueries = 50
+
+    val statements = lines.split(";");
+
+    if (statements.size < 8) {
+      println("Expecting at least 8 statements to create denorm table")
+      System.exit(-1)
+    }
+    val sc = new SharkContext(System.getenv("MASTER"), "unusedFrameworkName")
+    SharkEnv.sc = sc
+
+    val denormCreateStatements = statements.slice(0, 8)
+    for (stmt <- denormCreateStatements) {
+      sc.sql(stmt)
+    }
+    val queries = statements.slice(8, statements.length)
+    val pool = new ScheduledThreadPoolExecutor(20) // Up to 20 outstanding queries
+    var cumulativeDelay = 0;
+    while (System.currentTimeMillis() - t0 < startDelayMs) {
+      Thread.sleep(100)
+    }
+    var iterNum = 0
+    for (q <- queries) {
+      iterNum = iterNum + 1
+      pool.schedule(new QueryLaunchRunnable(sc, q), cumulativeDelay, TimeUnit.MILLISECONDS)
+      if (iterNum < warmUpQueries) {
+        cumulativeDelay += warmUpDelayMs
+      } else {
+        cumulativeDelay += delayMs
+      }
+    }
+    println("Finished denormalized table creation")
+  }
+}
+
+class QueryLaunchRunnable(sc : SharkContext, query: String) extends Runnable with Logging {
+  def run() {
+    logInfo("THREAD:" + Thread.currentThread().getId() + " QUERY:" + query.replace("\n", ""))
+    sc.sql(query)
+  }
+}
